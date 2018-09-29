162329f96b0d8d3c73498a21ba7fce0d9798a295
Nik <minscky.d123@yandex.ru>
2018-09-29 22:34:33 +0300

    Init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+ // var arr1 = equation.split('/ * x^2 | * x /');
+  var arr = equation.split(' * x^2 ').join(' ').split(' * x ').join(' ').split(' ');
+
+  const a = arr[0];
+  const b = arr[1]+arr[2];
+  const c = arr[3]+arr[4];
+  const dis =  Math.pow(b, 2) - (4*a*c);
+ 
+  let x = solve(a, b, dis);
+  x = sort(x);
+
+  return x;
+}
+
+function solve(a, b, dis) {
+  return [Math.round((-1*b-Math.sqrt(dis))/(2*a)), Math.round((-1*b+Math.sqrt(dis))/(2*a))];
+}
+
+function sort(x) {
+  return x.sort((left, right) => left - right);
 }
